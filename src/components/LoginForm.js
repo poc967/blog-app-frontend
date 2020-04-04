f58bc3d1a1cfe6d0707c2cd066108111dcb08720
Patrick O'Connor <poc.967@gmail.com>
2020-04-03 23:24:56 -0400

    WIP login form component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,19 @@ import '../styles/LoginForm.css'
 class LoginForm extends Component {
 
     state = {
-        value: ''
+        email: "",
+        password: ""
+    }
+
+    handleValueChange = (e) => {
+        const target = e.target
+        const value = target.name === 'email' ? target.value : target.value
+        const key = target.name
+        console.log(e.target.value)
+
+        this.setState({
+            [key]: value
+        })
     }
 
     render() {
@@ -13,11 +25,15 @@ class LoginForm extends Component {
                 <h2>Log In</h2>
                 <form>
                     <input type="text"
-                        value={this.state.value}
-                        placeholder="email" />
+                        name="email"
+                        checked={this.state.email}
+                        placeholder="email"
+                        onChange={this.handleValueChange} />
                     <input type="text"
-                        value={this.state.value}
-                        placeholder="password" />
+                        name="password"
+                        value={this.state.password}
+                        placeholder="password"
+                        onChange={this.handleValueChange} />
                     <input type="submit" value="Sign In" />
                 </form>
             </div>
