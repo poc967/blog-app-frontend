02e295dcca5f20da5e5645f22144ca6b0b8d5548
Patrick O'Connor <poc.967@gmail.com>
2020-04-12 12:01:18 -0400

    added not found component and implemented swtich logic is app router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import '../styles/App.css'
+
+//import required components
 import Nav from './Nav'
 import Home from './Home'
 import SignUpModal from './SignUpModal'
 import LogInModal from './LoginModal'
 import Footer from './Footer'
-import '../styles/App.css'
+import NotFound from './NotFound'
 
 class App extends Component {
 
@@ -32,13 +35,15 @@ class App extends Component {
           <Nav
             user={this.state.user}
             handleLogIn={this.handleLogIn} />
-          <Route exact path='/' component={Home} />
-          {/* <Route path='/about' component={About} /> */}
-          <Route path='/signup' component={SignUpModal} />
-          <Route path='/login' component={LogInModal} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            {/* <Route path='/about' component={About} /> */}
+            <Route path='/signup' component={SignUpModal} />
+            <Route path='/login' component={LogInModal} />
+            <Route component={NotFound} />
+          </Switch>
           <Footer />
         </BrowserRouter>
-
 
       </div>
     )
---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/NotFound.css'
+
+const NotFound = () => {
+    return (
+        <div className="notFound">
+            <h1>404</h1>
+            <span>Page not found</span>
+        </div>
+    )
+}
+
+export default NotFound
