f861e40f56110c4ff10ae608523f8b057fb02f78
Patrick O'Connor <poc.967@gmail.com>
2020-04-21 22:23:32 -0400

    added delete button

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,6 +7,10 @@ const Post = (props) => {
             <h2>{props.title}</h2>
             <h3>By: {props.author}</h3>
             <p><strong>{props.category} </strong>{props.body}</p>
+            <div className="postFooter">
+                <button className="button-neutral">Edit</button>
+                <button className="button-danger" onClick={() => props.handlePostDelete(props.post._id)}>Delete</button>
+            </div>
         </li>
     )
 }
--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,25 +1,28 @@
 import React from 'react'
+import '../styles/PostContainer.css'
 import Post from './Post'
 
-const Posts = (props) => {
+const PostContainer = (props) => {
 
     const postsList = props.posts
 
     let post = postsList.map((post) => {
         return <Post key={post._id}
+            post={post}
             title={post.title}
             author={post.author}
             category={post.category}
-            body={post.post} />
+            body={post.post}
+            handlePostDelete={props.handlePostDelete} />
+
     })
 
+
     return (
-        <div className="posts">
-            <ul >
-                {post}
-            </ul>
-        </div>
+        <ul>
+            {post}
+        </ul>
     )
 }
 
-export default Posts
+export default PostContainer
